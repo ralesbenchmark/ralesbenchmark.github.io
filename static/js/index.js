6b31c37ec226a5e735ef4ec1315707df6a51736a
JMZAM <jmz@stanford.edu>
2023-08-12 22:30:19 -0700

    add NLU table

--- static/js/index.js
+++ static/js/index.js
@@ -76,3 +76,32 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+$(document).ready(function() {
+  $('.result-table th').each(function() {
+    $(this).addClass('sort-button');
+    $(this).click(function() {
+      const table = $(this).parents('table').eq(0);
+      const rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+      this.asc = !this.asc;
+      if (!this.asc) {
+        rows.reverse();
+      }
+      for (let i = 0; i < rows.length; i++) {
+        table.append(rows[i]);
+      }
+      table.find('th').removeClass('asc desc');
+      $(this).addClass(this.asc ? 'asc' : 'desc');
+    });
+  });
+  function comparer(index) {
+    return function(a, b) {
+      const valA = getCellValue(a, index);
+      const valB = getCellValue(b, index);
+      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
+    };
+  }
+  function getCellValue(row, index) {
+    return $(row).children('td').eq(index).text();
+  }
+});
